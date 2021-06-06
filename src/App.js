4ecb9cf221064562e99547a9b8a782ca691ec99b
saiteja <saiteja3118@gmail.com>
2021-06-06 14:44:17 +0530

    chaged app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        This is edited by Parameshwari
+        This is made by some one else
+	i dont care who he is
+	This is edited by Parameshwari
+	This is changed by teja
 
     </div>
   );
