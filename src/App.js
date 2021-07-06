48c115217ec6ffa0e2e2b6e73d5e3d9cb9d74594
44donin <44donin@gmail.com>
2021-07-06 07:47:31 +0530

    added the home directory in the src file and pakage.json have upated all the required pakages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "../src/home/home"
+
 
 function App() {
   return (
     <div className="App">
-
-        This is made by some one else
-	i dont care who he is
-	This is edited by Parameshwari
-	This is changed by teja
-	jingle bels jingle bells gingle all the way
-	jhonny jhony yes papa
-	  eating suger no papa
-	  telling lies no papa
-	  open you mouth haha ha
-	  twinkl twinkle little star
-
-	  how i wonder what you are
-	  up above the world so high
-	  like a daimond in the sky 
-
+      <Home/>
     </div>
-  );
-}
+    );
+};
 
 export default App;
