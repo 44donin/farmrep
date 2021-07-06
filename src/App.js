b93626ea63d3e6a84ce43bf3eb06c0a861bde0e1
44donin <44donin@gmail.com>
2021-07-06 12:07:04 +0530

    added farmerpage.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from "../src/home/home"
-
+import Farmerpage from './openpage/farmerpage';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/farmer" component={Farmerpage} />        
+      </Router>
+      
     </div>
     );
 };
---
+++ src/openpage/farmerpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Farmerpage extends Component {
+    render() {
+        return (
+            <div>
+               Farmer gives details
+            </div>
+        );
+    }
+}
+
+export default Farmerpage;
